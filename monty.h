72d729ab384e6303b2cc64cc83666ca894bbd2f4
Ruffshok <111015150+Ruffshok@users.noreply.github.com>
2022-12-15 10:01:45 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -44,6 +44,7 @@ instruction_t *parse_line(char *line);
 void free_stack(stack_t *head);
 char *get_opcode(char *string);
 int is_int(char *str);
+int main(int argc, char **argv);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
